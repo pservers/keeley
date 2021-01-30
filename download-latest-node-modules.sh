88e9d5e24d7440df9a271f2c6aaeaf9f814c0f5f
fpemud <fpemud@sina.com>
2021-01-30 19:56:15 +0800

    fix

--- download-latest-node-modules.sh
+++ download-latest-node-modules.sh
@@ -1,9 +1,33 @@
 #!/bin/sh -e
+
 DSTDIR=$(dirname $0)/keeley/static
+#REGISTRY=
+REGISTRY="http://registry.npm.taobao.org"
 
 rm -rf "$DSTDIR/node_modules"
 
-#npm install --only=production --prefix="$DSTDIR" @syncfusion/ej2-filemanager
-npm install --only=production --prefix="$DSTDIR" --registry="http://registry.npm.taobao.org" @syncfusion/ej2-filemanager
-#cnpm install --only=production --prefix="$DSTDIR" "@syncfusion/ej2-filemanager"
+# generate a fake package.json (should we have a package.json?)
+npm init --prefix="$DSTDIR" -y > "$DSTDIR/package.json"
+sed -i "1,2d" "$DSTDIR/package.json"
+sed -i 's/\"description": \"\"/\"description\": \"keely\"/g' "$DSTDIR/package.json"
+
+# registry switch
+if [ ! -z $REGISTRY ] ; then
+    REGISTRY_ARG="--registry=$REGISTRY"
+else
+    REGISTRY_ARG=""
+fi
+
+# install packages
+npm install --no-fund --no-save --only=production --prefix="$DSTDIR" $REGISTRY_ARG bufferutil
+npm install --no-fund --no-save --only=production --prefix="$DSTDIR" $REGISTRY_ARG utf-8-validate
+npm install --no-fund --no-save --only=production --prefix="$DSTDIR" $REGISTRY_ARG @syncfusion/ej2-filemanager
+
+# remove temp files
+rm -f "$DSTDIR/package.json"
+rm -f "$DSTDIR/package-lock.json"
+
+
+
+
 #yarn --cwd "$DSTDIR" --registry=http://registry.npm.taobao.org add @syncfusion/ej2-vue-filemanager
