6fe86d31d51b552b8a6e5766a4c5fe8e9c65da15
fpemud <fpemud@sina.com>
2021-01-31 15:18:39 +0800

    fix

--- run-bundler.sh
+++ run-bundler.sh
@@ -3,6 +3,7 @@
 # we use a download>generate>delete process because the files npm downloaded are too large (133M) so
 # that it is not good to commit them into a version control repository.
 
+SRCDIR=$(dirname $0)/keeley/bundle-src
 DSTDIR=$(dirname $0)/keeley/static
 #REGISTRY=
 REGISTRY="http://registry.npm.taobao.org"
@@ -10,9 +11,9 @@ REGISTRY="http://registry.npm.taobao.org"
 rm -rf "$DSTDIR/node_modules"
 
 # generate a fake package.json (should we have a package.json?)
-npm init --prefix="$DSTDIR" -y > "$DSTDIR/package.json"
-sed -i "1,2d" "$DSTDIR/package.json"
-sed -i 's/\"description": \"\"/\"description\": \"keely\"/g' "$DSTDIR/package.json"
+#npm init --prefix="$DSTDIR" -y > "$DSTDIR/package.json"
+#sed -i "1,2d" "$DSTDIR/package.json"
+#sed -i 's/\"description": \"\"/\"description\": \"keely\"/g' "$DSTDIR/package.json"
 
 # registry switch
 if [ ! -z $REGISTRY ] ; then
@@ -22,11 +23,11 @@ else
 fi
 
 # install packages
-npm install --no-fund --no-save --only=production --prefix="$DSTDIR" $REGISTRY_ARG bufferutil utf-8-validate @syncfusion/ej2-filemanager
-npm install --no-fund --no-save --only=production --prefix="$DSTDIR" $REGISTRY_ARG webpack webpack-cli css-loader style-loader
+npm install --no-fund --save --only=production --prefix="$SRCDIR" $REGISTRY_ARG bufferutil utf-8-validate @syncfusion/ej2-filemanager
+npm install --no-fund --save --only=production --prefix="$SRCDIR" $REGISTRY_ARG webpack webpack-cli css-loader style-loader
 
 # bundle
-cat <<-EOF > "$DSTDIR/webpack.config.js"
+cat <<-EOF > "$SRCDIR/webpack.config.js"
 	module.exports = {
 	  entry: './main.js',
 	  output: {
@@ -34,11 +35,11 @@ cat <<-EOF > "$DSTDIR/webpack.config.js"
 	  },
 	}
 	EOF
-webpack --env production --env min --config "$DSTDIR/webpack.config.js"
+webpack --env production --env min --config "$SRCDIR/webpack.config.js"
 
 # remove temp files
 if [ "$1" != "--keep" ] ; then
-	rm -f "$DSTDIR/package.json"
-	rm -f "$DSTDIR/package-lock.json"
-	rm -f "$DSTDIR/webpack.config.js"
+#	rm -f "$DSTDIR/package.json"
+#	rm -f "$DSTDIR/package-lock.json"
+#	rm -f "$DSTDIR/webpack.config.js"
 fi
