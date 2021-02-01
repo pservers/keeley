86f82856a75aaa71c2c316cb7c1a3d518daa0f41
fpemud <fpemud@sina.com>
2021-02-01 22:20:18 +0800

    fix

--- keeley/bundle-src/main.js
+++ keeley/bundle-src/main.js
@@ -1,17 +1,15 @@
-import '@syncfusion/ej2-base/styles/material.css';
-import '@syncfusion/ej2-icons/styles/material.css';
-import '@syncfusion/ej2-inputs/styles/material.css';
-import '@syncfusion/ej2-popups/styles/material.css';
-import '@syncfusion/ej2-buttons/styles/material.css';
-import '@syncfusion/ej2-splitbuttons/styles/material.css';
-import '@syncfusion/ej2-navigations/styles/material.css';
-import '@syncfusion/ej2-layouts/styles/material.css';
-import '@syncfusion/ej2-grids/styles/material.css';
-import '@syncfusion/ej2-filemanager/styles/material.css';
+import './node_modules/@syncfusion/ej2-base/styles/material.css';
+import './node_modules/@syncfusion/ej2-icons/styles/material.css';
+import './node_modules/@syncfusion/ej2-inputs/styles/material.css';
+import './node_modules/@syncfusion/ej2-popups/styles/material.css';
+import './node_modules/@syncfusion/ej2-buttons/styles/material.css';
+import './node_modules/@syncfusion/ej2-splitbuttons/styles/material.css';
+import './node_modules/@syncfusion/ej2-navigations/styles/material.css';
+import './node_modules/@syncfusion/ej2-layouts/styles/material.css';
+import './node_modules/@syncfusion/ej2-grids/styles/material.css';
+import './node_modules/@syncfusion/ej2-filemanager/styles/material.css';
 
-<title>{{ SITE_NAME }}</title>
-
-import { FileManager } from '@syncfusion/ej2-filemanager';
+import { FileManager } from './node_modules/@syncfusion/ej2-filemanager';
 let fm = new FileManager({
     ajaxSettings: {
         url: 'api/FileManager/FileOperations',
